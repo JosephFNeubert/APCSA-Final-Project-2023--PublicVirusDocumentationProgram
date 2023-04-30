534dc7cd16c441533372ee26d78b9ee7e944e55f
JosephFNeubert <joeyfneubert@gmail.com>
2023-04-29 20:04:00 -0700

    Beginning of project. Sets up both files.

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+/*  Runner class for the virus website documentation program. Uses additional "ReportsDatabase" class
+ *  to correctly execute all necessary functions. Program stores reports of virus websites in a ArrayList of string arrays,
+ *  allows new reports to be made, and allows for anybody to find a read other reports.
+ */
+//Import all necessary packages
+import java.util.Scanner;
+
+//Runner
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("");
+    }
+}
